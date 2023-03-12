f9a097e434467d95a7704a79fa58bd8bf9aee30b
RGamz_PC <rgamzat@live.com>
2023-03-12 14:02:56 +0100

    finished overlay on gear

---
+++ js/dropdown.js
@@ -0,0 +1,23 @@
+const gearBlock = document.querySelector("#gear__block");
+const drop = document.querySelector("#drop");
+const leftMarg = 5.6875 + "rem";
+
+gearBlock.addEventListener("mouseover", e => {
+    drop.style.display = "block";
+    drop.style.left = leftMarg
+
+    drop.addEventListener("mouseover", e => {
+        drop.style.display = "block";
+        drop.style.left = leftMarg
+    })
+
+    drop.addEventListener("mouseleave", e => {
+        drop.style.display = "none";
+    })
+})
+
+gearBlock.addEventListener("mouseleave", e => {
+    drop.style.display = "none";
+})
+
+

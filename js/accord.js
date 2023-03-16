482c42bf0e68219899dad1f8d3f1bf8da85eeb59
RGamz_PC <rgamzat@live.com>
2023-03-16 07:46:10 +0100

    working on adaptive team

--- js/accord.js
+++ js/accord.js
@@ -1,12 +1,21 @@
 $(document).ready(() => {
-    $('.team__name-section').click(e => {
-        e.preventDefault();
+    
+    $('.team__name-section').on('click', function() {
+        $(this).addClass('dropcheck');
+        const block = $('.dropcheck').next();
 
-        $('.team__dropdown').css({
-            'height': '100%',
-            'overflow': 'visible'
-        });
-
-       
+        if (block.hasClass('active')) {
+            block.stop(true, true).animate({
+                'height': '0'
+            });
+            block.removeClass('active');
+            $(this).removeClass('dropcheck');
+        } else {
+            block.stop(true, true).animate({
+                'height': '100%'
+            });
+            block.addClass('active');
+        }
     });
- });
+
+});

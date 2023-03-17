9a596f220a5f3f8d5280df6aa566acbe3abd1599
RGamz_PC <rgamzat@live.com>
2023-03-17 09:09:13 +0100

    accordeon finished

--- js/accord.js
+++ js/accord.js
@@ -1,21 +1,42 @@
-$(document).ready(() => {
-    
-    $('.team__name-section').on('click', function() {
-        $(this).addClass('dropcheck');
-        const block = $('.dropcheck').next();
 
-        if (block.hasClass('active')) {
-            block.stop(true, true).animate({
-                'height': '0'
-            });
-            block.removeClass('active');
-            $(this).removeClass('dropcheck');
-        } else {
-            block.stop(true, true).animate({
-                'height': '100%'
-            });
-            block.addClass('active');
-        }
-    });
+const openItem = item => {
+    const container = item.closest(".team__item");
+    const contentBlock = container.find(".team__dropdown")
+    const textBlock = contentBlock.find(".team__dropdown-wrapper");
+    const reqHeight = textBlock.height();
 
-});
+    container.addClass("active");
+    contentBlock.height(reqHeight);
+}
+
+const closeEveryItem = container => {
+    const items = container.find(".team__dropdown");
+    const itemContainer = container.find(".team__item");
+
+
+    itemContainer.removeClass("active");
+    items.height(0);
+}
+
+$(".team__name-section").click(e => {
+    const $this = $(e.currentTarget);
+    const container = $this.closest(".team")
+    const elemContainer = $this.closest(".team__item");
+    const triangleDown = $this.find(".triangle__down")
+    const triangleUp = $this.find(".triangle__up")
+
+
+    if (elemContainer.hasClass("active")) {
+        closeEveryItem(container);
+        $(triangleDown).css('display', 'inline');
+        $(triangleUp).css('display', 'none');
+    } else {
+        closeEveryItem(container);
+        openItem($this);
+        $(triangleDown).css('display', 'none');
+        $(triangleUp).css('display', 'inline');
+    } 
+
+})
+
+

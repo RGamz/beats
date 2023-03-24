390ae5eaa62cbfd5a61bdb309dead865c8189b1b
RGamz_PC <rgamzat@live.com>
2023-03-24 20:48:01 +0100

    working on acc

---
+++ js/colors-accord.js
@@ -0,0 +1,30 @@
+
+const openColor = item => {
+    const hiddenColor = item.find(".slide__item-content");
+    hiddenColor.addClass("slide__item-content_active")
+}
+
+const closeEveryColor = container => {
+    const items = container.find(".slide__item");
+    const content = items.find(".slide__item-content")
+
+    content.removeClass("slide__item-content_active")
+}
+
+$(".slide__button").click(e => {
+    e.preventDefault();
+    const $this = $(e.currentTarget);
+    const item = $this.closest(".slide__item");
+    const openedColor = item.hasClass("slide__item-content_active")
+    const elemContainer = $this.closest(".slide__list");
+
+    if (openedColor) {
+        closeEveryColor(elemContainer);
+    } else {
+        closeEveryColor(elemContainer);
+        openColor(item);
+    } 
+
+})
+
+
--- js/modal-form.js
+++ js/modal-form.js
@@ -1,13 +1,13 @@
-const openButton = document.querySelector("#sendButton");
-const modal = document.querySelector("#modal__form");
-const closeBtn = document.querySelector("#closeBtn__form");
+// const openButton = document.querySelector("#sendButton");
+// const modal = document.querySelector("#modal__form");
+// const closeBtn = document.querySelector("#closeBtn__form");
 
-openButton.addEventListener("click", e => {
-  e.preventDefault();
-  modal.style.display = "flex";
-})
+// openButton.addEventListener("click", e => {
+//   e.preventDefault();
+//   modal.style.display = "flex";
+// })
 
-closeBtn.addEventListener("click", e => {
-  e.preventDefault();
-  modal.style.display = "none";
-})
+// closeBtn.addEventListener("click", e => {
+//   e.preventDefault();
+//   modal.style.display = "none";
+// })
